5b5c34a0abb3a8909dbfafc1d397ad8dbed1cfec
OfficalGM <22172055+OfficalGM@users.noreply.github.com>
2018-11-18 14:19:28 +0800

    update

--- contracts/Auth.sol
+++ contracts/Auth.sol
@@ -2,9 +2,18 @@ pragma solidity ^0.4.22;
 
 
 contract Auth {
-  address public Address;
+    mapping(uint256=>Tree) public tree;
+    uint256 public TreeNumber;
+    struct Tree{
+        string RootHash;
+    }
+    
     function verify( bytes32 hash, uint8 v, bytes32 r, bytes32 s) public pure returns(address retAddr) {
         retAddr= ecrecover(hash, v, r, s);
         return retAddr;
+    }
+    function setTree(string _roothash) public returns(bool) {
+        tree[TreeNumber++]=Tree(_roothash);
+        return true;
     }
 }
